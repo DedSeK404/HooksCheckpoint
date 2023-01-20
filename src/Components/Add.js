b49f806790cdb3167c8e8cf5ef6338000b6d897c
MARWEN BENNACEUR <116744749+DedSeK404@users.noreply.github.com>
2023-01-20 14:43:48 +0100

    Update Add.js

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,6 +1,6 @@
 import React,{ useState } from "react";
-import { Button, ListGroup } from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 
@@ -31,61 +31,66 @@ export function Add({handelAdd}) {
     }
     handelAdd(data)
   }
-  const [search, setSearch] = useState("");
+  
   return (
-  <div style={{display:"flex",flexWrap:"wrap",gap:"10px"}} >
-    <form  style={{display:"flex",flexDirection:"column",gap:"10px"}} onSubmit={handleSubmit}>
+  <div style={{display:"flex",flexWrap:"wrap",gap:"10px", padding:"20px"}} >
+    <form style={{display:"flex",flexDirection:"column",gap:"20px"}}  onSubmit={handleSubmit}>
       <h1 style={{color:"#e50814"}}>Add your Movie</h1>
-      <div style={{display:"flex", gap:'40px'}} >
-      <p style={{color:"#e50814"}}>Type Movie Name:</p>
-        <input style={{width:"100%",height:"fit-content"}} name="name"  value={name} onChange={(e) => setName (e.target.value)}/>
+      <div style={{display:"flex",}} >
+        <InputGroup  value={name} onChange={(e) => setName (e.target.value)} className="mb-3">
+        <InputGroup.Text style={{borderRadius:"0%"}}  id="inputGroup-sizing-default">
+        Movie Name
+        </InputGroup.Text>
+        <Form.Control
+        style={{borderRadius:"0%"}}
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        </InputGroup>
+      
         </div>
-        <div style={{display:"flex", gap:'20px'}}>
-        <p style={{color:"#e50814"}}>Type Movie Description:</p>
-        <input style={{width:"100%",height:"fit-content"}} name="description"  value={description} onChange={(e) => setDescription (e.target.value)} />
+        
+        <div style={{display:"flex", }}>
+        <InputGroup value={description} onChange={(e) => setDescription (e.target.value)} className="mb-3">
+        <InputGroup.Text style={{borderRadius:"0%"}} id="inputGroup-sizing-default">
+        Movie Description
+        </InputGroup.Text>
+        <Form.Control
+        style={{borderRadius:"0%"}}
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        </InputGroup>
+    
         </div>
-        <div style={{display:"flex", gap:'60px'}}>
-        <p style={{color:"#e50814"}}>Type Poster Url:</p>
-        <input style={{width:"100%",height:"fit-content"}} name="imgUrl"  value={imgUrl} onChange={(e) => setImgUrl (e.target.value)} />
+        <div style={{display:"flex",}}>
+        <InputGroup  value={imgUrl} onChange={(e) => setImgUrl (e.target.value)} className="mb-3">
+        <InputGroup.Text style={{borderRadius:"0%"}} id="inputGroup-sizing-default">
+        Poster Url
+        </InputGroup.Text>
+        <Form.Control
+        style={{borderRadius:"0%"}}
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        </InputGroup>
+        
         </div>
-        <div style={{display:"flex", gap:'40px'}}>
-        <p style={{color:"#e50814"}}>Type Movie Rating:</p>
-        <input style={{width:"100%",height:"fit-content"}} name="rating"  value={rating} onChange={(e) => setRating (e.target.value)} />
+        <div style={{display:"flex",}}>
+        <InputGroup  value={rating} onChange={(e) => setRating (e.target.value)} className="mb-3">
+        <InputGroup.Text style={{borderRadius:"0%"}} id="inputGroup-sizing-default">
+        Movie Rating
+        </InputGroup.Text>
+        <Form.Control
+        style={{borderRadius:"0%"}}
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        </InputGroup>
+        
         </div>
       <button style={{width:"100%"}} className="btn_add" >Add Movie</button>
       </form>
-      {/* <input
-        style={{width:"100%"}}
-        name="name"
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search By Name/Rating"
-      />
-      {
-        list.filter((el) => {
-          return search.toLocaleLowerCase() === ""
-            ? el
-            : el.name.toLocaleLowerCase().includes(search) ||
-                el.rating.includes(search);
-        }).map((a)=><div key={a.id}>
-           <Card  style={{ width: '10rem', backgroundColor:"black", borderRadius:"0%" }}>
-      <Card.Img variant="top" src={a.imgUrl} />
-      <Card.Body>
-        <Card.Title style={{  color:"#e50814" }}>Movie Title:</Card.Title>
-        <Card.Title style={{  color:"white" }}>{a.name}</Card.Title>
-        <Card.Text style={{  color:"#e50814" }}>
-          Description:
-        </Card.Text>
-        <Card.Text style={{  color:"white" }}>
-          {a.description}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item style={{  backgroundColor:"#101010", color:"white" }} >Rating: {a.rating}</ListGroup.Item>
-      </ListGroup>
-      <Button style={{borderRadius:"0%"}} variant="danger">Watch Movie</Button>
-    </Card>
-          </div>) */}
-      {/* } */}
     </div>
    
   );
